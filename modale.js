f49f1900849c2f296557887d6b2551f6b192bb5b
quentin laparra <quentin.laparra@gmail.com>
2021-06-21 18:06:12 +0200

    add modal

---
+++ modale.js
@@ -0,0 +1,18 @@
+// Recup button and div.modale
+let button = document.querySelector(".modaleshow"); // selection du bouton
+let modal = document.querySelector(".modal"); // selection de la div
+let closebutton = document.querySelector(".modalclose"); // selection du bouton
+// add evement onclick at button
+button.onclick = function () {
+    modal.style.display = "block"
+}
+// evement: passer de cacher à visible
+closebutton.onclick = function () {
+    modal.style.display = "none"
+}
+// si click exterieur de la modale
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
