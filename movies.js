437a39f8e6a75f05958a1ffe7953998b164c796d
quentin laparra <quentin.laparra@gmail.com>
2021-06-28 23:18:21 +0200

    add function get data for mives in the category

--- movies.js
+++ movies.js
@@ -1,33 +1,114 @@
-// je me connecte à l'api
-const url = 'http://localhost:8000/api/v1/titles/'
-// je recupere la promesse
-async function getMovies(url) {
 
-    let movies = await fetch(url)
-        .then(resp => resp.json())
-        .then(resp => { return resp })
-    return movies.results
+const fantasy_url = "http://localhost:8000/api/v1/titles/?genre=Fantasy&sort_by=-imdb_score";
+const horror_url = "http://localhost:8000/api/v1/titles/?genre=Horror&sort_by=-imdb_score";
+const mystery_url = "http://localhost:8000/api/v1/titles/?genre=Mystery&sort_by=-imdb_score";
+const best_movies_url = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score"
+
+
+async function getCatMovies(url) {
+    let movie = await fetch(url)
+        .then(response => response.json())
+        .then(response => { return response })
+    let nextUrl = movie.next
+    let nextMovie = await fetch(nextUrl)
+        .then(response => response.json())
+        .then(response => { return response })
+
+    let finalTab = movie.results.concat(nextMovie.results)
+    return finalTab
 }
-// je recupere les dicos de chaque films
-getMovies(url).then(resp => {
-    for (let movie of resp) {
-        console.log(movie.title)
-    }
+getCatMovies(best_movies_url).then(resp => {
+    let image_url0 = resp[0].image_url
+    let image_url1 = resp[1].image_url
+    let image_url2 = resp[2].image_url
+    let image_url3 = resp[3].image_url
+    let image_url4 = resp[4].image_url
+    let image_url5 = resp[5].image_url
+    let image_url6 = resp[6].image_url
+    let affichage_image_url0 = document.querySelector("#img1")
+    let affichage_image_url1 = document.querySelector("#img2")
+    let affichage_image_url2 = document.querySelector("#img3")
+    let affichage_image_url3 = document.querySelector("#img4")
+    let affichage_image_url4 = document.querySelector("#img5")
+    let affichage_image_url5 = document.querySelector("#img6")
+    let affichage_image_url6 = document.querySelector("#img7")
+    affichage_image_url0.src = image_url0
+    affichage_image_url1.src = image_url1
+    affichage_image_url2.src = image_url2
+    affichage_image_url3.src = image_url3
+    affichage_image_url4.src = image_url4
+    affichage_image_url5.src = image_url5
+    affichage_image_url6.src = image_url6
 })
 
+getCatMovies(fantasy_url).then(resp => {
+    let image_url0 = resp[0].image_url
+    let image_url1 = resp[1].image_url
+    let image_url2 = resp[2].image_url
+    let image_url3 = resp[3].image_url
+    let image_url4 = resp[4].image_url
+    let image_url5 = resp[5].image_url
+    let image_url6 = resp[6].image_url
+    let affichage_image_url0 = document.querySelector("#img8")
+    let affichage_image_url1 = document.querySelector("#img9")
+    let affichage_image_url2 = document.querySelector("#img10")
+    let affichage_image_url3 = document.querySelector("#img11")
+    let affichage_image_url4 = document.querySelector("#img12")
+    let affichage_image_url5 = document.querySelector("#img13")
+    let affichage_image_url6 = document.querySelector("#img14")
+    affichage_image_url0.src = image_url0
+    affichage_image_url1.src = image_url1
+    affichage_image_url2.src = image_url2
+    affichage_image_url3.src = image_url3
+    affichage_image_url4.src = image_url4
+    affichage_image_url5.src = image_url5
+    affichage_image_url6.src = image_url6
+})
 
-// creation d'une fonction best movie?
-    // recupere les infos du meilleur film
-        // url du meilleurs film
-        //"imdb_score": "9.6" + "votes": 961
+getCatMovies(horror_url).then(resp => {
+    let image_url0 = resp[0].image_url
+    let image_url1 = resp[1].image_url
+    let image_url2 = resp[2].image_url
+    let image_url3 = resp[3].image_url
+    let image_url4 = resp[4].image_url
+    let image_url5 = resp[5].image_url
+    let image_url6 = resp[6].image_url
+    let affichage_image_url0 = document.querySelector("#img15")
+    let affichage_image_url1 = document.querySelector("#img16")
+    let affichage_image_url2 = document.querySelector("#img17")
+    let affichage_image_url3 = document.querySelector("#img18")
+    let affichage_image_url4 = document.querySelector("#img19")
+    let affichage_image_url5 = document.querySelector("#img20")
+    let affichage_image_url6 = document.querySelector("#img21")
+    affichage_image_url0.src = image_url0
+    affichage_image_url1.src = image_url1
+    affichage_image_url2.src = image_url2
+    affichage_image_url3.src = image_url3
+    affichage_image_url4.src = image_url4
+    affichage_image_url5.src = image_url5
+    affichage_image_url6.src = image_url6
+})
 
-// creation d'un fonction all best movies???
-    // recupere les infos des meilleurs film de chaque categorie
-        // url des meilleurs films de chaqus categories
-            //"imdb_score": "9.6"
-
-// creation d'une fonction pour les categorie??? 
-    // recupere les infos des  films d'une  categorie
-        //url = Fantasy
-        //url = Horror
-        //url = Mystery
+getCatMovies(mystery_url).then(resp => {
+    let image_url0 = resp[0].image_url
+    let image_url1 = resp[1].image_url
+    let image_url2 = resp[2].image_url
+    let image_url3 = resp[3].image_url
+    let image_url4 = resp[4].image_url
+    let image_url5 = resp[5].image_url
+    let image_url6 = resp[6].image_url
+    let affichage_image_url0 = document.querySelector("#img22")
+    let affichage_image_url1 = document.querySelector("#img23")
+    let affichage_image_url2 = document.querySelector("#img24")
+    let affichage_image_url3 = document.querySelector("#img25")
+    let affichage_image_url4 = document.querySelector("#img26")
+    let affichage_image_url5 = document.querySelector("#img27")
+    let affichage_image_url6 = document.querySelector("#img28")
+    affichage_image_url0.src = image_url0
+    affichage_image_url1.src = image_url1
+    affichage_image_url2.src = image_url2
+    affichage_image_url3.src = image_url3
+    affichage_image_url4.src = image_url4
+    affichage_image_url5.src = image_url5
+    affichage_image_url6.src = image_url6
+})
