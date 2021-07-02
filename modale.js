e3fde8d57ade1849452fa4cbbf8a680c7bcc07d5
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-07-02 19:01:40 +0200

    create modal for the categroies

--- modale.js
+++ modale.js
@@ -1,20 +1,49 @@
-// Recup button and div.modale
-let button = document.querySelector(".modaleshow"); // selection du bouton
-let modal = document.querySelector(".modal"); // selection de la div
-let closebutton = document.querySelector(".modalclose"); // selection du bouton
-// add evement onclick at button
-button.onclick = function () {
-    modal.style.display = "block"
+
+const fantasy_url = "http://localhost:8000/api/v1/titles/?genre=Fantasy&sort_by=-imdb_score";
+const horror_url = "http://localhost:8000/api/v1/titles/?genre=Horror&sort_by=-imdb_score";
+const mystery_url = "http://localhost:8000/api/v1/titles/?genre=Mystery&sort_by=-imdb_score";
+const best_movies_url = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score"
+
+
+async function getCatMovies(url) {
+    let movie = await fetch(url)
+        .then(response => response.json())
+        .then(response => { return response })
+    let nextUrl = movie.next
+    let nextMovie = await fetch(nextUrl)
+        .then(response => response.json())
+        .then(response => { return response })
+    let finalTab = movie.results.concat(nextMovie.results)
+    return finalTab
 }
-// evement: passer de cacher à visible
-closebutton.onclick = function () {
-    modal.style.display = "none"
-}
-// si click exterieur de la modale
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+
+async function creatMoviesCat(list_movies, classeCat) {
+    // recup le contenaire
+    let moviesByCat = document.querySelector(`.carrousel.${classeCat}`)
+    let div_movies = moviesByCat.querySelectorAll(".item")
+    for (let [i, movie] of div_movies.entries()) {
+        let affichage_image_url = movie.querySelector("img")
+        affichage_image_url.src = list_movies[i].image_url
+        let movie_infos = await getMovieInfo(list_movies[i].url)
+        movie.addEventListener("click", function () {
+            displayModal(movie_infos)
+        })
     }
 }
 
+getCatMovies(best_movies_url)
+    .then(resp => {
+        creatMoviesCat(resp, "top-movies");
+    })
 
+getCatMovies(fantasy_url).then(resp => {
+    creatMoviesCat(resp, "fantasy");
+})
+
+getCatMovies(horror_url).then(resp => {
+    creatMoviesCat(resp, "horror");
+})
+
+getCatMovies(mystery_url).then(resp => {
+    creatMoviesCat(resp, "mystery");
+})
