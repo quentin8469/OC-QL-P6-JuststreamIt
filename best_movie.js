ffc7d73ef93be932eae0fdd20dc92ff8787c9661
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-07-02 19:03:57 +0200

    refactoring function best movie

--- best_movie.js
+++ best_movie.js
@@ -17,31 +17,13 @@ best_movie
             let url_movie = response.results[0].url
             let movie = await getMovieInfo(url_movie)
             //modif du DOM avec les datas du film
-            let affichage_actors = document.querySelector("#actors")
-            let affichage_directors = document.querySelector("#real")
-            let affichage_genres = document.querySelector("#genres")
-            let affichage_imdb_score = document.querySelector("#imdb")
-            let affichage_title = document.querySelector("#title")
-            let affichage_votes = document.querySelector("#rated")
-            let affichage_year = document.querySelector("#date")
-            let affichage_big_title = document.querySelector(".titre")
-            let affichage_duration = document.querySelector("#time")
-            let affichage_long_description = document.querySelector("#resum")
-            let affichage_countries = document.querySelector("#country")
-            let affichage_image_url = document.getElementById("img0");
-            //affichage dans html
-            affichage_actors.innerText = movie.actors
-            affichage_directors.innerText = movie.directors
-            affichage_genres.innerText = movie.genres
-            affichage_imdb_score.innerText = movie.imdb_score
-            affichage_title.innerText = movie.title
-            affichage_votes.innerText = movie.votes
-            affichage_year.innerText = movie.year
-            affichage_big_title.innerText = movie.title
-            affichage_duration.innerText = movie.duration
-            affichage_long_description.innerText = movie.long_description
-            affichage_countries.innerText = movie.countries
+            let affichage_image_url = document.querySelector("#img0")
             affichage_image_url.src = movie.image_url
+            document.querySelector(".titre").innerText = movie.title
+            button.addEventListener("click", function () {
+                displayModal(movie)
+            })
+
         } catch (err) {
             console.log(err);
         }
